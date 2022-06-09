f07fbd2ee7804f063c129555495945aaa52ad72d
JakeDafro <105757563+JakeDafro@users.noreply.github.com>
2022-06-08 19:01:14 -0600

    1c Lab in HelloWorld

--- Basics/001 Conventions and Syntax/HelloWorld.cs
+++ Basics/001 Conventions and Syntax/HelloWorld.cs
@@ -1,12 +1,22 @@
-//Use this file to store your work to demonstrate your understanding of operators
-//Make sure your work is different from the instructors examples or your peers
 using System;
 
 public class HelloWorld
 {
     public static void Main(string[] args)
     {
-        //Change "HW" to "Hello World"
-        Console.WriteLine("HW");
+		
+		
+        string[] soda = {"Coke", "Rootbeer", "Sprite", "Pepsi", "Mountain Dew"};
+        foreach (string can in soda){
+			int i = 0;
+			while (i < soda.Length){
+            Console.WriteLine(can);
+			i++;
+            
+            }
+		}
+		for (int p = 0; p <= 12; p = p + 3){
+                Console.WriteLine(p);
+        }
     }    
 }
